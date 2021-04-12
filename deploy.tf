e47ec502f0176a1f2c192e69bd391f87c1518e56
Anh Quach <anh.quachquituan@halliburton.com>
2021-04-12 15:47:36 +0700

    First init for simple logic app

---
+++ deploy.tf
@@ -0,0 +1,14 @@
+locals {
+  tenant_id         = "c14b46fc-2780-4bee-bcfa-e3f5a1c337b9"
+  subscription_name = "AZURE-SUB-TA"
+  subscription_id   = "d0767e87-e2a6-4477-a762-768ce1367057"
+  location = "southeastasia"
+}
+
+module "terraform-azure-logic-app" {
+  source              = "git::https://github.com/aq-terraform-modules/logic-app.git"
+
+  resource_group_name = "${subscription_name}-logicapp"
+  location            = local.location
+  logic_app_name      = "logicapp"
+}
---
+++ main.tf
@@ -0,0 +1,26 @@
+# Provider configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+provider "azurerm" {
+  tenant_id       = local.tenant_id
+  subscription_id = local.subscription_id
+  features {
+    
+  }
+}
+
+# Backend configuration
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate"
+    storage_account_name  = "tfstate4445"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
